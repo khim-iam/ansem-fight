70d005a1400609fc24bc27c3dd0f8de58fa213cf
JoeruCodes <joymedi8143@gmail.com>
2024-04-29 15:58:39 +0530

    ipro

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from 'react';
-// Import assets and howler
 import { Howl } from 'howler';
 import ansem from '../assets/start.png'; 
 import ansemPunch from '../assets/idlee.png';
@@ -61,110 +60,140 @@ const PunchesConfig = [{
 function HomePage() {
   const [wifAmount, setWifAmount] = useState(0);
   const [punches, setPunches] = useState(0);
-  const [intervalId, setIntervalId] = useState(null);
   const [currentImageArray, setCurrentImageArray] = useState(imageSets.default);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const soundRef = useRef(null);
-
+  const intervalRef = useRef(null);
+  const soundRef = useRef({
+    punch: new Howl({ src: [sounds[SoundTypes.PUNCH]], volume: 0.5 }),
+    win: new Howl({ src: [sounds[SoundTypes.WIN]], volume: 0.5 }),
+    lose: new Howl({ src: [sounds[SoundTypes.LOSE]], volume: 0.5 }),
+    bell: new Howl({ src: [sounds[SoundTypes.BELL]], volume: 0.5 }),
+    tier3: new Howl({ src: [sounds[SoundTypes.TIER3]], volume: 0.5 }),
+    background: new Howl({ src: [sounds.background], loop: true, volume: 0.1 })
+  });
+  const containerRef = useRef(null);
   useEffect(() => {
-      if (!soundRef.current) {
-          soundRef.current = new Howl({ src: [sounds.background], autoplay: true, loop: true, volume: 0.1 });
-      }
-      if (!soundRef.current.playing()) {
-          soundRef.current.play();
-      }
-      return () => clearInterval(intervalId);
-  }, [intervalId]);
+    const handlePunchSound = () => {
+      setTimeout(() => {
+        containerRef.current.classList.add('cameraShake');
+        setTimeout(() => {
+          containerRef.current.classList.remove('cameraShake');
+        }, 10); // remove the class after 75ms
+      }, 10); // add the class 200ms before the punch sound plays
+    };
+  
+    soundRef.current.punch.on('play', (id, seek) => {
+      setTimeout(handlePunchSound, seek - 1); // add cameraShake 100ms before playback
+    }); 
+  
+    return () => {
+      soundRef.current.punch.off('play', handlePunchSound);
+    };
+  }, [soundRef, containerRef]);
+  useEffect(() => {
+    playSound('background');  // Play background music when component mounts
 
-  const playSound = (soundType) => {
-    return new Promise((resolve, reject) => {
-      const sound = new Audio(sounds[soundType]);
-      sound.play();
-      sound.onended = resolve;
-      sound.onerror = reject;
-    });
+    return () => {
+        clearInterval(intervalRef.current);
+        soundRef.current.background.stop();  // Ensure background sound is stopped on unmount
+    };
+  }, []);
+
+  const playSound = (soundType, forcePlay = false) => {
+    if (soundType === 'background' && soundRef.current[soundType].playing()) {
+        return;
+    }
+
+    if (forcePlay || soundType !== SoundTypes.PUNCH) {
+        soundRef.current[soundType].stop();
+    }
+
+    soundRef.current[soundType].play();
   };
+
   const handleImageUpdate = (maxRuns, imageSet, delay, npunch) => {
     let runCount = 0;
-    let currentIndex = 0;
-    clearInterval(intervalId);
+    clearInterval(intervalRef.current);
     const id = setInterval(async () => {
       if (runCount >= maxRuns) {
-        clearInterval(id);
-        setIntervalId(null);
+        clearInterval(intervalRef.current);
+        intervalRef.current = null;
         setCurrentImageArray(imageSets.result);
         setCurrentImageIndex(npunch > 35 ? 1 : 0);
         await playSound(npunch > 35 ? SoundTypes.WIN : SoundTypes.LOSE);
         handleDefault();
         return;
       }
-      for (let i = 0; i < imageSet.length; i++) {
-        if (imageSet.length === 2) { // 2-image set (punch and stance)
-          if (i === 0) { // play punch sound for the first image (punch)
-            await playSound(SoundTypes.PUNCH);
-          }
-          setTimeout(() => {
-            setCurrentImageIndex(i % imageSet.length);
-          }, i * 750); // adjusted timing for 2-image set
-        } else {
-          if (i < imageSet.length - 1) { // play punch sound for all but the last image
-            await playSound(SoundTypes.PUNCH);
-          }
-          setTimeout(() => {
-            setCurrentImageIndex(i % imageSet.length);
-          }, i * 500); // flip through the images
-        }
+  
+      let currentImages = [...imageSet];
+      if (imageSet === imageSets.t2) {
+        // Randomly decide how many punches to use: 1 or 2
+        const numPunches = Math.random() < 0.5 ? 1 : 2;
+        const punches = shuffleArray([imageSet[1], imageSet[2]]).slice(0, numPunches);
+        currentImages = [imageSet[0], ...punches]; // Always include the stance image
       }
-      setPunches(p => p - 1);
+  
+      // Display the images with the randomized punches
+      for (let i = 0; i < currentImages.length; i++) {
+        setTimeout(() => {
+          
+          if (i < currentImages.length - 1) { // Play punch sound for all but the last image
+            setTimeout(() => playSound(SoundTypes.PUNCH), 2)
+          }
+          setCurrentImageIndex(i % currentImages.length);
+        }, i * 750);  // Timing for each image and sound
+      }
       runCount++;
-    }, 2000); // adjust the interval time to control the speed of the image flip sequence
-    setIntervalId(id);
+      setPunches(p => p - 1);
+    }, delay + 2000); // Overall interval delay
+    intervalRef.current = id;
   };
+  // Fisher-Yates (Knuth) shuffle function for array randomization
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
   const handleDefault = () => {
-      clearInterval(intervalId);
+      clearInterval(intervalRef.current);
       const id = setInterval(() => {
           setCurrentImageIndex(0);
           setCurrentImageArray(imageSets.default);
       }, 2000);
-      setIntervalId(id);
+      intervalRef.current = id;
   };
 
   const generatePunches = (minPunches, maxPunches) => {
     const x = Math.floor(Math.random() * (maxPunches - minPunches + 1)) + minPunches;
-    setPunches(x);  // This should only set the initial state
+    setPunches(x);
     return x;
   };
 
   const handleDeposit = () => {
       const inputWif = prompt("Enter WIF amount (positive number):");
       const wif = Number(inputWif);
-      if (wif > 0) {
+      if (!isNaN(wif) && wif > 0) {
           playSound(SoundTypes.BELL);
           setWifAmount(wif);
           let minPunches, maxPunches, imageArr;
 
           if (wif <= 40) {
-            if (wif === 1) {
-              ({minPunches, maxPunches, imageArr} = PunchesConfig[0]);
-            } else {
-              ({minPunches, maxPunches, imageArr} = PunchesConfig[1]);
-            }
-
-
-          const randPunches = generatePunches(minPunches, maxPunches);
-          setCurrentImageArray(imageArr);
-          handleImageUpdate(randPunches, imageArr, 0, randPunches);
+            ({minPunches, maxPunches, imageArr} = wif === 1 ? PunchesConfig[0] : PunchesConfig[1]);
+            const randPunches = generatePunches(minPunches, maxPunches);
+            setCurrentImageArray(imageArr);
+            handleImageUpdate(randPunches, imageArr, 0, randPunches);
           } else {
             ({minPunches, maxPunches, imageArr} = PunchesConfig[2]);
             playSound(SoundTypes.TIER3);
 
-          const randPunches = generatePunches(minPunches, maxPunches);
-          setCurrentImageArray(imageArr);
-          handleImageUpdate(randPunches, imageArr, 2000, randPunches);
-          
-        }
-  // Adjusted to use randPunches directly
+            const randPunches = generatePunches(minPunches, maxPunches);
+            setCurrentImageArray(imageArr);
+            handleImageUpdate(randPunches, imageArr, 2000, randPunches);
+          }
       } else {
           alert("Please enter a positive number for WIF amount.");
       }
@@ -172,9 +201,9 @@ function HomePage() {
 
   return (
       <>
-          <div className="image-container relative">
-              <img src={currentImageArray[currentImageIndex]} alt="Game character" />
-          </div>
+          <div ref={containerRef} className="image-container relative">
+  <img src={currentImageArray[currentImageIndex]} alt="Game character" />
+</div>  
           <h1 className="custom-heading text-6xl text-[#2196F3]">Ansem vs. Barney</h1>
           <div className="card custom-heading text-[25px]">
               <button onClick={handleDeposit}>Deposit WIF</button> 
