7163950294d842cda7f52c3132c21da4044ba155
Himanshu Kumar <imhimanshu369@gmail.com>
2024-05-21 05:07:09 +0530

    ngrok

--- src/helpers/dataHandlers.js
+++ src/helpers/dataHandlers.js
@@ -1,4 +1,8 @@
 import sign from "jwt-encode";
+
+const ngrokUrl = 'https://6070-103-21-125-86.ngrok-free.app';
+//const ngrokUrl = 'http://localhost:5000';
+
 export const handleSendData = async (randPunches, wif, referredBy, wallet) => {
   console.log('randPunches:', randPunches);
   console.log('wif:', wif);
@@ -20,7 +24,9 @@ export const handleSendData = async (randPunches, wif, referredBy, wallet) => {
     win: 1 // Set to 1 if the user won
   };
   try {
-    const finishResponse = await fetch('http://localhost:5000/api/finish', {
+    //const finishResponse = await fetch('http://localhost:5000/api/finish', {
+    const finishResponse = await fetch(ngrokUrl+'/api/finish', {
+
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -43,7 +49,9 @@ export const handleSendData = async (randPunches, wif, referredBy, wallet) => {
 
 export const sendData = async (userData) => {
   try {
-    const response = await fetch('http://localhost:5000/api/wallet', {
+   // const response = await fetch('http://localhost:5000/api/wallet', {
+    const response = await fetch(ngrokUrl+'/api/wallet', {
+
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
@@ -67,11 +75,24 @@ export const sendData = async (userData) => {
 
 export const getLeaderboardData = async (updateLeaderboard) => {
   try {
-    const leaderboardResponse = await fetch('http://localhost:5000/api/leaderboard');
+    //const leaderboardResponse = await fetch('http://localhost:5000/api/leaderboard');
+   // const leaderboardResponse = await fetch(ngrokUrl+'/api/leaderboard');
+    const leaderboardResponse = await fetch(ngrokUrl + '/api/leaderboard', {
+      headers: {
+        'ngrok-skip-browser-warning': 'true' // or any other value
+      }
+    });
     if (!leaderboardResponse.ok) {
       throw new Error('Failed to fetch leaderboard data');
     }
+    //console.log('leaderboardResponse-  ', leaderboardResponse);
+    // const textResponse = await leaderboardResponse.text();
+    // console.log('Raw Response:', textResponse);
+
+
     const leaderboardData = await leaderboardResponse.json();
+    //const leaderboardData = await JSON.parse(textResponse);
+
     console.log('Leaderboard Data:', leaderboardData);
     // Perform calculations for top 10 players based on the received data
     
