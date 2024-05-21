392e7113e18bd37c57a87d7746203c50dda61b04
Himanshu Kumar <imhimanshu369@gmail.com>
2024-05-21 11:07:08 +0530

    wasm in vite.config

--- vite.config.js
+++ vite.config.js
@@ -21,11 +21,55 @@
 //     },
 //   },
 // });
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "tailwindcss";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
-import path from "path";
+
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "tailwindcss";
+// import { nodePolyfills } from "vite-plugin-node-polyfills";
+// import path from "path";
+
+// // Helper function to resolve relative paths
+// const resolvePath = (p) => path.resolve(__dirname, p);
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     nodePolyfills({
+//       globals: {
+//         Buffer: true,
+//         global: true,
+//         process: true,
+//       },
+//       protocolImports: true,
+//     }),
+//   ],
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss()],
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       // These aliases allow you to use absolute paths in your imports
+//       "@": resolvePath("src"),
+//       "@assets": resolvePath("src/assets"),
+//       "@components": resolvePath("src/components"),
+//       "@styles": resolvePath("src/pages"),
+//       "@howler": resolvePath("src/howler"),
+//     },
+//   },
+// });
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import path from 'path';
 
 // Helper function to resolve relative paths
 const resolvePath = (p) => path.resolve(__dirname, p);
@@ -41,6 +85,8 @@ export default defineConfig({
       },
       protocolImports: true,
     }),
+    wasm(),
+    topLevelAwait(),
   ],
   css: {
     postcss: {
@@ -49,12 +95,12 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      // These aliases allow you to use absolute paths in your imports
-      "@": resolvePath("src"),
-      "@assets": resolvePath("src/assets"),
-      "@components": resolvePath("src/components"),
-      "@styles": resolvePath("src/pages"),
-      "@howler": resolvePath("src/howler"),
+      '@': resolvePath('src'),
+      '@assets': resolvePath('src/assets'),
+      '@components': resolvePath('src/components'),
+      '@styles': resolvePath('src/pages'),
+      '@howler': resolvePath('src/howler'),
+      'ansem-wasm': resolvePath('wasm-ansem-fight/pkg'), // Add this line for aliasing ansem-wasm
     },
   },
 });
